62501fd316224cccf3f0845e45891cf6cc532e1c
netium <zb.public@outlook.com>
2022-01-03 20:28:21 +0800

    Implemented the +,-,*,/ operations.

--- CLox/vm.cpp
+++ CLox/vm.cpp
@@ -22,6 +22,7 @@ void freeVM() {
 static InterpretResult run() {
 #define READ_BYTE() (*vm.ip++)
 #define READ_CONSTANT() (vm.chunk->constants.values[READ_BYTE()])
+#define BINARY_OP(op) do { double b = pop(); double a = pop(); push (a op b); } while (false)
 
 	while (true) {
 #ifdef DEBUG_TRACE_EXECUTION
@@ -42,6 +43,18 @@ static InterpretResult run() {
 			push(constant);
 			break;
 		}
+		case OP_ADD:
+			BINARY_OP(+);
+			break;
+		case OP_SUBTRACT:
+			BINARY_OP(-);
+			break;
+		case OP_MULTIPLY:
+			BINARY_OP(*);
+			break;
+		case OP_DIVIDE:
+			BINARY_OP(/ );
+			break;
 		case OP_NEGATE: 
 			push(-pop());
 			break;
@@ -55,6 +68,7 @@ static InterpretResult run() {
 	}
 #undef READ_BYTE
 #undef READ_CONSTANT
+#undef BINARY_OP
 }
 
 InterpretResult interpret(Chunk* chunk) {
