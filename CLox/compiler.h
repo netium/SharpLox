37396c24c5b93f6f338635a857fb5741e2daffd1
Bo Zhou <bo.zhou@cerence.com>
2022-01-04 18:30:07 +0800

    Implemented

--- CLox/compiler.h
+++ CLox/compiler.h
@@ -3,6 +3,8 @@
 #ifndef CLOX_COMPILER_H
 #define CLOX_COMPILER_H
 
+#include "object.h"
+#include "vm.h"
 #include "chunk.h"
 
 bool compile(const char* source, Chunk *chunk);
--- CLox/vm.c
+++ CLox/vm.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
 #include <stdarg.h>
+#include <string.h>
 
 #include "common.h"
 #include "compiler.h"
 #include "debug.h"
+#include "object.h"
+#include "memory.h"
 #include "vm.h"
 
 
@@ -11,6 +14,7 @@ VM vm;
 
 static Value peek(int distance);
 static bool isFalsey(Value value);
+static void concatenate();
 
 static void resetStack() {
 	vm.stackTop = vm.stack;
@@ -31,10 +35,11 @@ static void runtimeError(const char* format, ...) {
 
 void initVM() {
 	resetStack();
+	vm.objects = NULL;
 }
 
 void freeVM() {
-
+	freeObjects();
 }
 
 static InterpretResult run() {
@@ -84,9 +89,22 @@ static InterpretResult run() {
 		case OP_LESS:
 			BINARY_OP(BOOL_VAL, <);
 			break;
-		case OP_ADD:
-			BINARY_OP(NUMBER_VAL, +);
-			break;
+		case OP_ADD: 
+			{
+				if (IS_STRING(peek(0)) && IS_STRING(peek(1))) {
+					concatenate();
+				}
+				else if (IS_NUMBER(peek(0)) && IS_NUMBER(peek(1))) {
+					double b = AS_NUMBER(pop());
+					double a = AS_NUMBER(pop());
+					push(NUMBER_VAL(a + b));
+				} 
+				else {
+					runtimeError("Operands must be two numbers or two strings.");
+					return INTERPRET_RUNTIME_ERROR;
+				}
+				break;
+			}
 		case OP_SUBTRACT:
 			BINARY_OP(NUMBER_VAL, -);
 			break;
@@ -153,4 +171,20 @@ static Value peek(int distance) {
 
 static bool isFalsey(Value value) {
 	return IS_NIL(value) || (IS_BOOL(value) && !AS_BOOL(value));
+}
+
+static void concatenate() {
+	ObjString* b = AS_STRING(pop());
+	ObjString* a = AS_STRING(pop());
+
+	int length = a->length + b->length;
+
+	char* chars = ALLOCATE(char, length + 1);
+
+	memcpy(chars, a->chars, a->length);
+	memcpy(chars + a->length, b->chars, b->length);
+	chars[length] = '\0';
+
+	ObjString* result = takeString(chars, length);
+	push(OBJ_VAL(result));
 }
