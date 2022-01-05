f73b8237b53b58778c5c2d909c42631500a52f5e
netium <zb.public@outlook.com>
2022-01-05 23:08:17 +0800

    Init implementation

--- CLox/compiler.c
+++ CLox/compiler.c
@@ -34,6 +34,9 @@ typedef enum {
 typedef void (*ParseFn)(bool canAssign);
 
 typedef struct {
+	ObjFunction* function;
+	FunctionType type;
+
 	Local locals[UINT8_COUNT];
 	int localCount;
 	int scopeDepth;
@@ -50,6 +53,11 @@ typedef struct {
 	int depth;
 } Local;
 
+typedef enum {
+	TYPE_FUNCTION,
+	TYPE_SCRIPT
+} FunctionType;
+
 Parser parser;
 Compiler* current = NULL;
 Chunk* compilingChunk;
@@ -62,7 +70,7 @@ static void declaration();
 static ParseRule* getRule(TokenType type);
 
 static Chunk* currentChunk() {
-	return compilingChunk;
+	return &current->function->chunk;
 }
 
 static void errorAt(Token* token, const char* message) {
@@ -179,20 +187,29 @@ static void patchJump(int offset) {
 	currentChunk()->code[offset + 1] = jump & 0xff;
 }
 
-static void initCompiler(Compiler* compiler) {
+static void initCompiler(Compiler* compiler, FunctionType type) {
+	compiler->function = NULL;
+	compiler->type = type;
 	compiler->localCount = 0;
 	compiler->scopeDepth = 0;
+	compiler->function = newFunction();
 	current = compiler;
+	Local* local = &current->locals[current->localCount++];
+	local->depth = 0;
+	local->name.start = "";
+	local->name.length = 0;
 }
 
-static void endCompiler() {
+static ObjFunction *endCompiler() {
 	emitReturn();
+	ObjFunction* function = current->function;
 
 #ifdef DEBUG_PRINT_CODE
 	if (!parser.hadError) {
-		disassembleChunk(currentChunk(), "code");
+		disassembleChunk(currentChunk(), function->name != NULL ? function->name->chars : "script");
 	}
 #endif
+	return function;
 }
 
 static void beginScope() {
@@ -658,11 +675,10 @@ static void statement() {
 	}
 }
 
-bool compile(const char* source, Chunk *chunk) {
+ObjFunction *compile(const char* source) {
 	initScanner(source);
 	Compiler compiler;
-	initCompiler(&compiler);
-	compilingChunk = chunk;
+	initCompiler(&compiler, TYPE_SCRIPT);
 
 	parser.hadError = false;
 	parser.panicMode = false;
@@ -673,7 +689,7 @@ bool compile(const char* source, Chunk *chunk) {
 		declaration();
 	}
 
-	endCompiler();
+	ObjFunction *function = endCompiler();
 
-	return !parser.hadError;
+	return parser.hadError ? NULL : function;
 }
--- CLox/compiler.h
+++ CLox/compiler.h
@@ -7,6 +7,6 @@
 #include "vm.h"
 #include "chunk.h"
 
-bool compile(const char* source, Chunk *chunk);
+ObjFunction *compile(const char* source);
 
 #endif
